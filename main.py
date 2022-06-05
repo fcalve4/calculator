8ff37ab9bb45741db905ba0782713ccb59d72b10
fcalve4 <100451879+fcalve4@users.noreply.github.com>
2022-06-05 12:39:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
 #Calculator by @fcalve4 on GitHub
+from math import sqrt
 import time
+
 def addition():
-    print("Input your first number. (integer)")
+    print("Time to add!")
+    time.sleep(1)
+    print("Input your first number.")
     first = input()
     time.sleep(1)
-    print("Input your second number. (integer)")
+    print("Input your second number.")
     second = input()
     time.sleep(1)
-    answer = int(first) + int(second)
+    answer = float(first) + float(second)
     return answer
 
 def subtraction():
-    print("Input your first number. (integer)")
+    print("Time to subtract!")
+    time.sleep(1)
+    print("Input your first number.")
     first = input()
     time.sleep(1)
-    print("Input your second number. (integer)")
+    print("Input your second number.")
     second = input()
     time.sleep(1)
-    answer = int(first) - int(second)
+    answer = float(first) - float(second)
     return answer
 
 def multiplication():
-    print("Input your first number. (integer)")
+    print("Time to multiply!")
+    time.sleep(1)
+    print("Input your first number.")
     first = input()
     time.sleep(1)
-    print("Input your second number. (integer)")
+    print("Input your second number.")
     second = input()
     time.sleep(1)
-    answer = int(first) * int(second)
+    answer = float(first) * float(second)
     return answer
+
 def division():
-    print("Input your first number. (integer)")
+    print("Time to divide!")
+    time.sleep(1)
+    print("Input your first number.")
     first = input()
     time.sleep(1)
-    print("Input your second number. (integer)")
+    print("Input your second number.")
     second = input()
     time.sleep(1)
-    answer = int(first) / int(second)
+    answer = float(first) / float(second)
     return answer
 
 def average():
@@ -52,18 +63,43 @@ def average():
     time.sleep(1)
     answer = 0
     for num in lst:
-        answer += int(num)
+        answer += float(num)
     answer /= count
     return answer
 
 def exponent():
-    print("Please select your base number. (integer)")
-    base = int(input())
+    print("Please select your base number.")
+    base = float(input())
     time.sleep(1)
     print("What power would you like to raise " + str(base) + " to?")
-    exp = int(input())
+    exp = float(input())
     time.sleep(1)
     answer = base ** exp
+    return answer
+
+def square_root():
+    print("Please input the number you would like to take the root of.")
+    num = float(input())
+    time.sleep(1)
+    answer = sqrt(num)
+    return answer
+
+def slope():
+    print("Let's find the slope between two points.")
+    time.sleep(1)
+    print("Input your first 'x' value.")
+    x1 = float(input())
+    time.sleep(1)
+    print("Input your first 'y' value.")
+    y1 = float(input())
+    time.sleep(1)
+    print("Input your second 'x' value.")
+    x2 = float(input())
+    time.sleep(1)
+    print("Input your second 'y' value.")
+    y2 = float(input())
+    time.sleep(1)
+    answer = ((y1 - y2) / (x1 - x2))
     return answer
 
 if __name__ == "__main__":
@@ -71,7 +107,16 @@ if __name__ == "__main__":
     print("-Calculator-")
     time.sleep(1)
     while True:
-        print("Select an Operation\n'a' for Addition\n's' for Subtraction\n'm' for Multiplication\n'd' for Division\n'v' for Average\n'e' for Exponent\nPress 'q' to exit")
+        print("Select an Operation")
+        print("'a' for Addition")
+        print("'s' for Subtraction")
+        print("'m' for Multiplication")
+        print("'d' for Division")
+        print("'v' for Average")
+        print("'e' for Exponent")
+        print("'r' for Square Root")
+        print("'z' for Slope")
+        print("Press 'q' to exit")
         op = input()
         time.sleep(1)
         if op == "a":
@@ -90,12 +135,20 @@ if __name__ == "__main__":
             answer = division()
             print("The answer is: " + str(answer) + ".")
             break
-        elif op == "avg":
+        elif op == "v":
             answer = average()
             print("The answer is: " + str(answer) + ".")
             break
         elif op == "e":
             answer = exponent()
+            print("The answer is: " + str(answer) + ".")
+            break
+        elif op == "r":
+            answer = square_root()
+            print("The answer is: " + str(answer) + ".")
+            break
+        elif op == "z":
+            answer = slope()
             print("The answer is: " + str(answer) + ".")
             break
         elif op == "q":
